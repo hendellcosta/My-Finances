36f5b2069b147967f1be974f5129e8bafdbabfc3
Hendell Petersen <hendell_petersen@hotmail.com>
2021-09-18 09:13:14 +0100

    - to .

--- scripts.js
+++ scripts.js
@@ -121,12 +121,12 @@ const Utils = {
     },
 
     formatDate(date) {
-        const splittedDate = date.split("-")
+        const splittedDate = date.split(".")
         return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
     },
 
     formatCurrency(value) {
-        const signal = Number(value) < 0 ? "-" : ""
+        const signal = Number(value) < 0 ? "." : ""
 
         value = String(value).replace(/\D/g, "")
 
